3f564dc9eec2e0575e5c8526b15230cc62d2ae1c
Josh <joshleehogan@gmail.com>
2022-07-20 20:14:18 -0500

    Fix Spinner

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -20,3 +20,11 @@ const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
+
+const abi = [
+    {
+        inputs:[],
+        stateMutability: "nonpayable",
+        type: "constructor",
+    },
+]
